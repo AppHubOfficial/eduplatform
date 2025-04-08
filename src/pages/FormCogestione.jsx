e5f57b8e2bc2b6f349d9baa6d1b232e6d27861f7
AppHubOfficial <AppHub2024@gmail.com>
2025-04-08 15:26:05 +0200

    Update FormCogestione.jsx

--- src/pages/FormCogestione.jsx
+++ src/pages/FormCogestione.jsx
@@ -41,6 +41,7 @@ const styleModal = {
 export default function PrenotazioneCogestione() {
 
     const [disableOraDAria, setDisableOraDAria] = useState(false);
+    const [disableStudio, setDisableStudio] = useState(false);
     const [open, setOpen] = useState(false);
     const [isLoading, setIsLoading] = useState(false);
     const [errorMessage, setErrorMessage] = useState("");
@@ -83,20 +84,21 @@ export default function PrenotazioneCogestione() {
     });
 
     ///////////////// FORM FIELDS /////////////////
+
     const formFields = [
         { label: 'Nome', name: 'nome', type: 'input', required: true },
         { label: 'Cognome', name: 'cognome', type: 'input', required: true },
         { label: 'Classe *', name: 'classe', type: 'selectClasse', required: true },
 
         { label: 'Mercoledì mattina', type: 'label' },
-        { label: 'Modulo 1', name: 'm1', ora: 'merc_mattina', type: 'selectAttivita', required: true },
-        { label: 'Modulo 2', name: 'm2', ora: 'merc_mattina', type: 'selectAttivita', required: true },
-        { label: 'Modulo 3', name: 'm3', ora: 'merc_mattina', type: 'selectAttivita', required: true },
+        { label: 'Modulo 1 | 8:10 - 9:52', name: 'm1', ora: 'm1', type: 'selectAttivita', required: true },
+        { label: 'Modulo 2 | 10:02 - 11:40', name: 'm2', ora: 'm2', type: 'selectAttivita', required: true },
+        { label: 'Modulo 3 | 11:50 - 13:34', name: 'm3', ora: 'm3', type: 'selectAttivita', required: true },
 
         { label: 'Giovedì mattina', type: 'label' },
-        { label: 'Modulo 1', name: 'g1', ora: 'giov_mattina', type: 'selectAttivita', required: true },
-        { label: 'Modulo 2', name: 'g2', ora: 'giov_mattina', type: 'selectAttivita', required: true },
-        { label: 'Modulo 3', name: 'g3', ora: 'giov_mattina', type: 'selectAttivita', required: true },
+        { label: 'Modulo 1 | 8:10 - 9:52', name: 'g1', ora: 'g1', type: 'selectAttivita', required: true },
+        { label: 'Modulo 2 | 10:02 - 11:40', name: 'g2', ora: 'g2', type: 'selectAttivita', required: true },
+        { label: 'Modulo 3 | 11:50 - 13:34', name: 'g3', ora: 'g3', type: 'selectAttivita', required: true },
 
         { label: `${["3", "4", "5"].includes(formData.classe.charAt(0)) ? "Giovedì" : "Mercoledì"} pomeriggio`, type: 'label' },
         { label: 'Modulo Pomeriggio', name: 'pomeriggio', ora: 'pomeriggio', type: 'selectAttivita', required: true },
@@ -105,29 +107,28 @@ export default function PrenotazioneCogestione() {
     ];
 
 
-
     const selectFields = [
-        { name: "assente", label: "Assente (tutta la mattina)", descr: "Non sarò presente.", ora: ["merc_mattina", "giov_mattina"] },
+        { name: "assente", label: "Assente (tutta la mattina)", descr: "Non sarò presente.", ora: ["m1", "m2", "m3", "g1", "g2", "g3"] },
         { name: "assente", label: "Assente", descr: "Non sarò presente.", ora: ["pomeriggio"] },
-        { name: "calcio_tutta_la_mattina", label: "Calcio (tutta la mattina)", descr: "Partita di calcio all'aperto. Le squadre verranno formate in anticipo.", ora: ["merc_mattina", "giov_mattina"] },
+        { name: "calcio_tutta_la_mattina", label: "Calcio (tutta la mattina)", descr: "Partita di calcio all'aperto. Le squadre verranno formate in anticipo.", ora: ["m1", "m2", "m3", "g1", "g2", "g3"] },
         { name: "calcio_non_torneo", label: "Calcio (Non torneo)", descr: "Partita di calcio amatoriale senza torneo, squadre organizzate prima.", ora: ["pomeriggio"] },
-        { name: "basket", label: "Basket", descr: "Partita di basket all'aperto con squadre organizzate prima dell'evento.", ora: ["merc_mattina", "giov_mattina", "pomeriggio"] },
-        { name: "pallavolo", label: "Pallavolo", descr: "Gioco di pallavolo all'aperto con squadre predefinite.", ora: ["merc_mattina", "giov_mattina", "pomeriggio"] },
-        { name: "ping_pong", label: "Ping Pong", descr: "Torneo di ping pong in aula attrezzata.", ora: ["merc_mattina", "giov_mattina", "pomeriggio"] },
-        { name: "cucina", label: "Cucina", descr: "Affiancamento al professor Casalegno nella preparazione della pasta per gli studenti.", ora: ["giov_mattina"] },
-        { name: "cucina_etnica", label: "Cucina Etnica (1€ ad assaggio)", descr: "Fiera gastronomica con piatti da tutto il mondo preparati da famiglie e docenti.", ora: ["giov_mattina", "pomeriggio"] },
-        { name: "make_up", label: "Make-up", descr: "Sessione di confronto sulle tecniche di trucco tra studenti e studentesse.", ora: ["merc_mattina", "giov_mattina", "pomeriggio"] },
-        { name: "croce_rossa", label: "Croce Rossa", descr: "Due corsi della Croce Rossa: malattie sessualmente trasmissibili e rischi della guida irresponsabile.", ora: ["merc_mattina", "giov_mattina", "pomeriggio"] },
-        { name: "forze_dell_ordine", label: "Forze dell'Ordine", descr: "Incontro informativo sulle carriere nelle forze dell'ordine.", ora: ["merc_mattina", "giov_mattina", "pomeriggio"] },
-        { name: "protezione_civile", label: "Protezione Civile", descr: "Dimostrazioni della Protezione Civile sui rischi della zona.", ora: ["merc_mattina", "giov_mattina", "pomeriggio"] },
-        { name: "programmazione", label: "Programmazione", descr: "Lezione su linguaggi di programmazione extra rispetto al programma ministeriale.", ora: ["merc_mattina", "giov_mattina", "pomeriggio"] },
-        { name: "cinema_anime", label: "Cinema/Anime", descr: "Visione di un'opera di cinema o anime con successiva riflessione critica.", ora: ["merc_mattina", "giov_mattina", "pomeriggio"] },
-        { name: "ballo", label: "Ballo", descr: "Lezioni di ballo moderno all'aperto con vari generi musicali.", ora: ["merc_mattina", "giov_mattina", "pomeriggio"] },
-        { name: "ludoteca", label: "Ludoteca", descr: "Sessione di gioco da tavolo con esperti della ludoteca di Castelnuovo Don Bosco.", ora: ["merc_mattina", "giov_mattina", "pomeriggio"] },
-        { name: "aula_di_studio", label: "Aula di Studio", descr: "Spazio dedicato allo studio o al riposo, disponibile solo per due moduli.", ora: ["merc_mattina", "giov_mattina", "pomeriggio"] },
-        { name: "pittura", label: "Pittura", descr: "Ritinteggiatura dell'aula LCF il 23/04/2025 e dell'aula 7 il 24/04/2025.", ora: ["merc_mattina", "giov_mattina", "pomeriggio"] },
-        { name: "ora_d_aria", label: "Ora d'aria", descr: "Momento di relax all'aperto.", ora: ["merc_mattina", "giov_mattina", "pomeriggio"] },
-
+        { name: "basket", label: "Basket", descr: "Partita di basket all'aperto con squadre organizzate prima dell'evento.", ora: ["m1", "m2", "m3", "g1", "g2", "g3", "pomeriggio"] },
+        { name: "pallavolo", label: "Pallavolo", descr: "Gioco di pallavolo all'aperto con squadre predefinite.", ora: ["m1", "m2", "m3", "g1", "g2", "g3", "pomeriggio"] },
+        { name: "ping_pong", label: "Ping Pong", descr: "Torneo di ping pong in aula attrezzata.", ora: ["m1", "m2", "m3", "g1", "g2", "g3", "pomeriggio"] },
+        { name: "cucina", label: "Cucina", descr: "Affiancamento al professor Casalegno nella preparazione della pasta per gli studenti.", ora: ["g3", "pomeriggio"] },
+        { name: "cucina_etnica", label: "Cucina Etnica (1€ ad assaggio)", descr: "Fiera gastronomica con piatti da tutto il mondo preparati da famiglie e docenti.", ora: ["g2"] },
+        { name: "make_up", label: "Make-up", descr: "Sessione di confronto sulle tecniche di trucco tra studenti e studentesse.", ora: ["m1", "m2", "m3", "g1", "g2", "g3", "pomeriggio"] },
+        { name: "croce_rossa", label: "Croce Rossa", descr: "Due corsi della Croce Rossa: malattie sessualmente trasmissibili e rischi della guida irresponsabile.", ora: ["m1", "m2", "m3", "g1", "g2", "g3", "pomeriggio"] },
+        { name: "forze_dell_ordine", label: "Forze dell'Ordine", descr: "Incontro informativo sulle carriere nelle forze dell'ordine.", ora: ["m1", "m2", "m3", "g1", "g2", "g3", "pomeriggio"] },
+        { name: "protezione_civile", label: "Protezione Civile", descr: "Dimostrazioni della Protezione Civile sui rischi della zona.", ora: ["m1", "m2", "m3", "g1", "g2", "g3", "pomeriggio"] },
+        { name: "programmazione", label: "Programmazione", descr: "Lezione su linguaggi di programmazione extra rispetto al programma ministeriale.", ora: ["g1", "g2", "g3", "pomeriggio"] },
+        { name: "cinema", label: "Cinema", descr: "Visione di un'opera di cinema con successiva riflessione critica.", ora: ["m1", "m2", "m3", "g1", "g2", "g3", "pomeriggio"] },
+        { name: "anime", label: "Anime", descr: "Visione di un anime con successiva riflessione critica.", ora: ["m1", "m2", "m3", "g1", "g2", "g3", "pomeriggio"] },
+        { name: "ballo", label: "Ballo", descr: "Lezioni di ballo moderno all'aperto con vari generi musicali.", ora: ["m1", "m2", "m3", "g1", "g2", "g3", "pomeriggio"] },
+        { name: "ludoteca", label: "Ludoteca", descr: "Sessione di gioco da tavolo con esperti della ludoteca di Castelnuovo Don Bosco.", ora: ["m1", "m2", "m3", "g1", "g2", "g3", "pomeriggio"] },
+        { name: "aula_di_studio", label: "Aula di Studio", descr: "Spazio dedicato allo studio o al riposo, disponibile solo per due moduli.", ora: ["m1", "m2", "m3", "g1", "g2", "g3", "pomeriggio"] },
+        { name: "pittura", label: "Pittura", descr: "Ritinteggiatura dell'aula LCF il 23/04/2025 e dell'aula 7 il 24/04/2025.", ora: ["m1", "m2", "m3", "g1", "g2", "g3", "pomeriggio"] },
+        { name: "ora_d_aria", label: "Ora d'aria", descr: "Momento di relax all'aperto.", ora: ["m1", "m2", "m3", "g1", "g2", "g3", "pomeriggio"] },
     ];
 
     //////////////////////////////////////////
@@ -258,6 +259,7 @@ export default function PrenotazioneCogestione() {
 
     useEffect(() => {
         setDisableOraDAria(Object.values(formData).includes("Ora d'aria"));
+        setDisableStudio(Object.values(formData).includes("Aula di Studio"));
     }, [formData]);
 
 
@@ -412,7 +414,10 @@ export default function PrenotazioneCogestione() {
                                                         <MenuItem
                                                             key={`${field.name}_${selectField.label}`}
                                                             value={selectField.label}
-                                                            disabled={selectField.label === "Ora d'aria" && disableOraDAria}
+                                                            disabled={
+                                                                (selectField.label === "Ora d'aria" && disableOraDAria) ||
+                                                                (!formData.classe.startsWith("5") && selectField.label === "Aula di Studio" && disableStudio)
+                                                            }
                                                         >
                                                             <Box display="flex" alignItems="center" justifyContent="space-between" width="100%">
                                                                 <span>{selectField.label}</span>
@@ -437,11 +442,6 @@ export default function PrenotazioneCogestione() {
 
                                 case 'radio':
                                     return (
-                                        // <FormControlLabel
-                                        //     key={`radio-${index}`}
-                                        //     control={<radio checked={formData.mangioScuola} name="mangioScuola" onChange={handleChange} />}
-                                        //     label="Mangio a scuola (2€)"
-                                        // />
 
                                         <FormControl fullWidth margin="normal" key={`formcontrol-${index}`}>
                                             <Box display="flex" alignItems="center" gap={3}>
